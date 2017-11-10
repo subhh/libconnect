faa4ab3c9bde84037cda4d6c5e25b2e6adb7a27b
Torsten Witt <torsten.witt@sub.uni-hamburg.de>
2017-11-10 09:13:54 +0100

    Coded cleaned and comments added.

--- ext_tables.php
+++ ext_tables.php
@@ -23,39 +23,14 @@ $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_dbis'] = 'p
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_ezb'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_ezb', 'FILE:EXT:'.$_EXTKEY.'/Configuration/FlexForms/ezb_flexform.xml');
 
+// Add typoscript
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'libconnect');
 
+// Add language
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_libconnect_domain_model_subject', 'EXT:libconnect/Resources/Private/Language/locallang_csh_tx_libconnect_domain_model_subject.xml');
+
+// Allow to store subject table on standard pages
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_libconnect_domain_model_subject');
-
-/*
-$TCA['tx_libconnect_domain_model_subject'] = array(
-    'ctrl' => array(
-        'title'    => 'Fachgebiet',
-        'label' => 'title',
-        'tstamp' => 'tstamp',
-        'crdate' => 'crdate',
-        'cruser_id' => 'cruser_id',
-        'default_sortby' => 'ORDER BY title',
-        //'dividers2tabs' => TRUE,
-        //'versioningWS' => 2,
-        //'versioning_followPages' => TRUE,
-        //'origUid' => 't3_origuid',
-        //'languageField' => 'sys_language_uid',
-        //'transOrigPointerField' => 'l10n_parent',
-        //'transOrigDiffSourceField' => 'l10n_diffsource',
-        'delete' => 'deleted',
-        'enablecolumns' => array(
-            'disabled' => 'hidden',
-            //'starttime' => 'starttime',
-            //'endtime' => 'endtime',
-
-        ),
-        'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/tx_libconnect_domain_model_subject.php',
-        'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Resources/Public/Icons/tx_libconnect_domain_model_subject.gif'
-    )
-);*/
-
 
 if (TYPO3_MODE == 'BE') {
     $TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_libconnect_dbis_wizicon'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/Wizicon/class.tx_libconnect_dbis_wizicon.php';
