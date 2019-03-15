995bf7e19764c0f45142b2c5364f1826b5b9dcd9
Torsten Witt <torsten.witt@sub.uni-hamburg.de>
2019-03-15 07:32:39 +0100

    Newline removed.

--- Classes/Domain/Repository/DbisRepository.php
+++ Classes/Domain/Repository/DbisRepository.php
@@ -34,7 +34,6 @@ namespace Sub\Libconnect\Domain\Repository;
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  *
  */
-
 if (!defined('TYPO3_COMPOSER_MODE') && defined('TYPO3_MODE')) {
 	require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('libconnect') . 'Resources/Private/Lib/Dbis.php');
 }
--- Resources/Private/Lib/Ezb.php
+++ Resources/Private/Lib/Ezb.php
@@ -38,7 +38,6 @@
  * @author Torsten Witt
  *
  */
-
 if (!defined('TYPO3_COMPOSER_MODE') && defined('TYPO3_MODE')) {
 	require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('libconnect') . 'Resources/Private/Lib/Xmlpageconnection.php');
 	require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('libconnect') . 'Resources/Private/Lib/Httppageconnection.php');
