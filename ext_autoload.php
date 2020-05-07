e9986ded6016427448cc59598a7865e4082dcec2
Torsten Witt <torsten.witt@sub.uni-hamburg.de>
2020-05-07 10:56:21 +0200

    Removed not used files.

--- ext_autoload.php
+++ ext_autoload.php
@@ -3,9 +3,7 @@ $extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('li
 $default = array(
     'Tx_libconnect_Resources_Private_Lib_Dbis' => $extensionPath . 'Resources/Private/Lib/Dbis.php',
     'Tx_libconnect_Resources_Private_Lib_Ezb' => $extensionPath . 'Resources/Private/Lib/Ezb.php',
-    'Tx_libconnect_Resources_Private_Lib_Zdb' => $extensionPath . 'Resources/Private/Lib/Zdb.php',
-    'Tx_libconnect_Resources_Private_Lib_Xmlpageconnection' => $extensionPath . 'Resources/Private/Lib/Xmlpageconnection.php',
-    'Tx_libconnect_Resources_Private_Lib_Httppageconnection' => $extensionPath . 'Resources/Private/Lib/Httppageconnection.php'
+    'Tx_libconnect_Resources_Private_Lib_Zdb' => $extensionPath . 'Resources/Private/Lib/Zdb.php'
 );
 
 return $default;
