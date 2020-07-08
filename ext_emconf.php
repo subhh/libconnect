56d86b4c0fc61e435b211d8ac9d577d73cad09ba
root <root@sr29.sub.uni-hamburg.de>
2020-07-08 10:55:56 +0200

    New version information.

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '7.0.2',
+  'version' => '7.0.3',
   'constraints' => 
   array (
     'depends' => 
