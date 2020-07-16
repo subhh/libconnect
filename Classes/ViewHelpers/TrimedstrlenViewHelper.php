843b8edda28350f0140395afc4527c75bb66286d
root <root@sr29.sub.uni-hamburg.de>
2020-07-16 13:28:00 +0200

    Changed comments

--- Classes/ViewHelpers/TrimedstrlenViewHelper.php
+++ Classes/ViewHelpers/TrimedstrlenViewHelper.php
@@ -2,20 +2,21 @@
 namespace Sub\Libconnect\ViewHelpers;
 
 /**
- * This class is a view helper that trim a string.
+ * This class is a view helper that count a trimed string.
  *
  * @package TYPO3
  * @subpackage Fluid
  * @version
  */
 
+
 class TrimedstrlenViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
 
     /**
-     * Returns trimmed string
+     * Returns count of trimmed string
      *
      * @param string $string
-     * @return string
+     * @return integer
      */
     public function render($string) {
         return strlen(trim($string));
