4004e1461cbdde06058f7a42d8bb4afdd44455bc
Christoph Lehmann <lehmann@networkteam.com>
2021-01-29 12:41:52 +0100

    Move plugin registration into userfunc

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,31 +4,32 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Libconnect',
-    'Dbis',
-    [
-        \Sub\Libconnect\Controller\DbisController::class => 'displayForm'
-    ],
-    // non-cacheable actions
-    [
-        \Sub\Libconnect\Controller\DbisController::class => 'displayDetail, displayList, displayMiniForm, displayNew'
-    ]
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Libconnect',
-    'Ezb',
-    [
-        \Sub\Libconnect\Controller\EzbController::class => 'displayForm, displayContact'
-    ],
-    // non-cacheable actions
-    [
-        \Sub\Libconnect\Controller\EzbController::class => 'displayDetail, displayList, displayMiniForm, displayNew, displayParticipantsForm'
-    ]
-);
-
 call_user_func(function() {
+
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Libconnect',
+        'Dbis',
+        [
+            \Sub\Libconnect\Controller\DbisController::class => 'displayForm'
+        ],
+        // non-cacheable actions
+        [
+            \Sub\Libconnect\Controller\DbisController::class => 'displayDetail, displayList, displayMiniForm, displayNew'
+        ]
+    );
+
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Libconnect',
+        'Ezb',
+        [
+            \Sub\Libconnect\Controller\EzbController::class => 'displayForm, displayContact'
+        ],
+        // non-cacheable actions
+        [
+            \Sub\Libconnect\Controller\EzbController::class => 'displayDetail, displayList, displayMiniForm, displayNew, displayParticipantsForm'
+        ]
+    );
+
     try {
         $enableDebugLog = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('libconnect', 'debug');
         if ($enableDebugLog) {
