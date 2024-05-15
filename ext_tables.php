347e579992bd2bdba9f6d6692ce7c1aea13b08ed
percolatorWitt <witt.torsten@googlemail.com>
2024-05-15 09:32:59 +0200

    Changed method to check access.

--- ext_tables.php
+++ ext_tables.php
@@ -1,8 +1,6 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3') or die('Access denied.');
 
 // Add language
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_libconnect_domain_model_subject', 'EXT:libconnect/Resources/Private/Language/locallang_csh_tx_libconnect_domain_model_subject.xml');
