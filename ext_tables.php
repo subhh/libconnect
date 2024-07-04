e25a6552b8e22bd7e8d68a4b05ef7ec7518fe6ea
percolatorWitt <witt.torsten@googlemail.com>
2024-07-04 13:49:50 +0200

    Removed deprecated functions.

--- ext_tables.php
+++ ext_tables.php
@@ -3,7 +3,7 @@
 defined('TYPO3') or die('Access denied.');
 
 // Add language
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_libconnect_domain_model_subject', 'EXT:libconnect/Resources/Private/Language/locallang_csh_tx_libconnect_domain_model_subject.xml');
+//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_libconnect_domain_model_subject', 'EXT:libconnect/Resources/Private/Language/locallang_csh_tx_libconnect_domain_model_subject.xml');
 
 // Allow to store subject table on standard pages
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_libconnect_domain_model_subject');
+//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_libconnect_domain_model_subject');
