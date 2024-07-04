34f42836ab9a0d0a7de7388144d8b24e60a03c9b
percolatorWitt <witt.torsten@googlemail.com>
2024-07-04 09:48:22 +0200

    Closed else.

--- Classes/Controller/EzbController.php
+++ Classes/Controller/EzbController.php
@@ -191,7 +191,7 @@ class EzbController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 	    if (!empty( $this->request->getQueryParams()['tx_libconnect_ezb'])) {
             $params = $this->request->getQueryParams()['tx_libconnect_ezb'];
             ArrayUtility::mergeRecursiveWithOverrule($params, $this->request->getParsedBody()['tx_libconnect_ezb']);
-        }else
+        } else {
             $params = $this->request->getQueryParams()['libconnect'];
             ArrayUtility::mergeRecursiveWithOverrule($params, $this->request->getParsedBody()['libconnect']);
         }
