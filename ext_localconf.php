a6cbe6dba4a8dc4e0e122fbb95e7d88f88ccb075
percolatorWitt <witt.torsten@googlemail.com>
2024-07-16 14:04:04 +0200

    Removed call_user_func.

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,42 +2,39 @@
 
 defined('TYPO3') or die('Access denied.');
 
-call_user_func(function() {
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Libconnect',
+    'Dbis',
+    [
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayForm'
+    ],
+    // non-cacheable actions
+    [
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayDetail, displayList, displayMiniForm, displayNew'
+    ]
+);
 
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'Libconnect',
-        'Dbis',
-        [
-            \Subhh\Libconnect\Controller\DbisController::class => 'displayForm'
-        ],
-        // non-cacheable actions
-        [
-            \Subhh\Libconnect\Controller\DbisController::class => 'displayDetail, displayList, displayMiniForm, displayNew'
-        ]
-    );
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Libconnect',
+    'Ezb',
+    [
+        \Subhh\Libconnect\Controller\EzbController::class => 'displayForm, displayContact'
+    ],
+    // non-cacheable actions
+    [
+        \Subhh\Libconnect\Controller\EzbController::class => 'displayDetail, displayList, displayMiniForm, displayNew, displayParticipantsForm'
+    ]
+);
 
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'Libconnect',
-        'Ezb',
-        [
-            \Subhh\Libconnect\Controller\EzbController::class => 'displayForm, displayContact'
-        ],
-        // non-cacheable actions
-        [
-            \Subhh\Libconnect\Controller\EzbController::class => 'displayDetail, displayList, displayMiniForm, displayNew, displayParticipantsForm'
-        ]
-    );
-
-    try {
-        $enableDebugLog = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('libconnect', 'debug');
-        if ($enableDebugLog) {
-            $GLOBALS['TYPO3_CONF_VARS']['Subhh']['Libconnect']['writerConfiguration'] = [
-                \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
-                    \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
-                        'logFileInfix' => 'libconnect',
-                    ]
+try {
+    $enableDebugLog = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('libconnect', 'debug');
+    if ($enableDebugLog) {
+        $GLOBALS['TYPO3_CONF_VARS']['Subhh']['Libconnect']['writerConfiguration'] = [
+            \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
+                \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
+                    'logFileInfix' => 'libconnect',
                 ]
-            ];
-        }
-    } catch (\Throwable $exception) {}
-});
+            ]
+        ];
+    }
+} catch (\Throwable $exception) {}
