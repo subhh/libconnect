d0bc4470bf9f6de4cd8f3dc77492f8367c4000fe
percolatorWitt <witt.torsten@googlemail.com>
2024-07-31 18:48:11 +0200

    Actions moved in seperate plugins to solve some problems.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,18 +1,84 @@
 <?php
 
-/* DBIS */
-
-$pluginSignature_dbis = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+/** DBIS **/
+/* DBIS List */
+$pluginSignature_dbis_list = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'Libconnect',
-    'Dbis',
-    'libconnect: dbis',
+    'DbisList',
+    'libconnect: DBIS List',
     'EXT:libconnect/Resources/Public/Icons/Wizard.gif'
 );
 
 // Add flexform for DBIS
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_dbis] = 'pi_flexform';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_dbis_list] = 'pi_flexform';
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_dbis, 'FILE:EXT:libconnect/Configuration/FlexForms/dbis_flexform.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_dbis_list, 'FILE:EXT:libconnect/Configuration/FlexForms/dbis_list_flexform.xml');
+
+/* DBIS Detail */
+$pluginSignature_dbis_detail = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Libconnect',
+    'DbisDetail',
+    'libconnect: DBIS Detail',
+    'EXT:libconnect/Resources/Public/Icons/Wizard.gif'
+);
+
+// Add flexform for DBIS
+//$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_dbis_detail] = 'pi_flexform';
+
+//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_dbis_detail, 'FILE:EXT:libconnect/Configuration/FlexForms/dbis_detail_flexform.xml');
+
+/* DBIS Search */
+$pluginSignature_dbis_search = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Libconnect',
+    'DbisSearch',
+    'libconnect: DBIS Advanced Search',
+    'EXT:libconnect/Resources/Public/Icons/Wizard.gif'
+);
+
+// Add flexform for DBIS
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_dbis_search] = 'pi_flexform';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_dbis_search, 'FILE:EXT:libconnect/Configuration/FlexForms/dbis_search_flexform.xml');
+
+/* DBIS Sidebar */
+$pluginSignature_dbis_sidebar = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Libconnect',
+    'DbisSidebar',
+    'libconnect: DBIS Sidebar',
+    'EXT:libconnect/Resources/Public/Icons/Wizard.gif'
+);
+
+// Add flexform for DBIS
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_dbis_sidebar] = 'pi_flexform';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_dbis_sidebar, 'FILE:EXT:libconnect/Configuration/FlexForms/dbis_search_flexform.xml');
+
+/* DBIS New */
+$pluginSignature_dbis_new = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Libconnect',
+    'DbisNew',
+    'libconnect: DBIS New Databases',
+    'EXT:libconnect/Resources/Public/Icons/Wizard.gif'
+);
+
+// Add flexform for DBIS
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_dbis_new] = 'pi_flexform';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_dbis_new, 'FILE:EXT:libconnect/Configuration/FlexForms/dbis_new_flexform.xml');
+
+/* DBIS Top */
+$pluginSignature_dbis_top = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Libconnect',
+    'DbisTop',
+    'libconnect: DBIS Top Databases',
+    'EXT:libconnect/Resources/Public/Icons/Wizard.gif'
+);
+
+// Add flexform for DBIS
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_dbis_top] = 'pi_flexform';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_dbis_top, 'FILE:EXT:libconnect/Configuration/FlexForms/dbis_top_flexform.xml');
+
 
 /** EZB **/
 /* EZB List */
@@ -40,3 +106,56 @@ $pluginSignature_ezb_detail = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::regis
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_ezb_detail] = 'pi_flexform';
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_ezb_detail, 'FILE:EXT:libconnect/Configuration/FlexForms/ezb_details_flexform.xml');
+
+
+/* EZB Search */
+$pluginSignature_ezb_search = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Libconnect',
+    'EzbSearch',
+    'libconnect: EZB Extended search',
+    'EXT:libconnect/Resources/Public/Icons/Wizard.gif'
+);
+
+// Add flexform
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_ezb_search] = 'pi_flexform';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_ezb_search, 'FILE:EXT:libconnect/Configuration/FlexForms/ezb_search_flexform.xml');
+
+/* EZB Sidebar */
+$pluginSignature_ezb_miniform = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Libconnect',
+    'EzbSidebar',
+    'libconnect: EZB Sidebar',
+    'EXT:libconnect/Resources/Public/Icons/Wizard.gif'
+);
+
+// Add flexform
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_ezb_miniform] = 'pi_flexform';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_ezb_miniform, 'FILE:EXT:libconnect/Configuration/FlexForms/ezb_sidebar_flexform.xml');
+
+/* EZB Contact */
+$pluginSignature_ezb_contact = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Libconnect',
+    'EzbContact',
+    'libconnect: EZB Contact',
+    'EXT:libconnect/Resources/Public/Icons/Wizard.gif'
+);
+
+// Add flexform
+//$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_ezb_contact] = 'pi_flexform';
+
+//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_ezb_contact, 'FILE:EXT:libconnect/Configuration/FlexForms/ezb_contact_flexform.xml');
+
+/* EZB New */
+$pluginSignature_ezb_new = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Libconnect',
+    'EzbNew',
+    'libconnect: EZB New Journals',
+    'EXT:libconnect/Resources/Public/Icons/Wizard.gif'
+);
+
+// Add flexform
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_ezb_new] = 'pi_flexform';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_ezb_new, 'FILE:EXT:libconnect/Configuration/FlexForms/ezb_new_flexform.xml');
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,18 +4,77 @@ defined('TYPO3') or die('Access denied.');
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Libconnect',
-    'Dbis',
+    'DbisList',
     [
-        \Subhh\Libconnect\Controller\DbisController::class => 'displayList, displayDetail, displayList, displayMiniForm, displayNew'
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayList'
     ],
     // non-cacheable actions
     [
-        \Subhh\Libconnect\Controller\DbisController::class => 'displayForm, displayDetail, displayList, displayMiniForm, displayNew'
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayList'
     ]
 );
 
-//\Subhh\Libconnect\Controller\EzbController::class => 'displayList, displayDetail, displayForm, displayMiniForm, displayContact, displayNew, displayParticipantsForm'
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Libconnect',
+    'DbisDetail',
+    [
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayDetail'
+    ],
+    // non-cacheable actions
+    [
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayDetail'
+    ]
+);
 
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Libconnect',
+    'DbisSearch',
+    [
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayForm'
+    ],
+    // non-cacheable actions
+    [
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayForm'
+    ]
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Libconnect',
+    'DbisSidebar',
+    [
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayMiniForm'
+    ],
+    // non-cacheable actions
+    [
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayMiniForm'
+    ]
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Libconnect',
+    'DbisNew',
+    [
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayNew'
+    ],
+    // non-cacheable actions
+    [
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayNew'
+    ]
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Libconnect',
+    'DbisTop',
+    [
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayTop'
+    ],
+    // non-cacheable actions
+    [
+        \Subhh\Libconnect\Controller\DbisController::class => 'displayTop'
+    ]
+);
+
+/** EZB **/
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Libconnect',
     'EzbList',
@@ -37,6 +96,66 @@ defined('TYPO3') or die('Access denied.');
     // non-cacheable actions
     [
         \Subhh\Libconnect\Controller\EzbController::class => 'displayDetail'
+    ]
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Libconnect',
+    'EzbSearch',
+    [
+        \Subhh\Libconnect\Controller\EzbController::class => 'displayForm'
+    ],
+    // non-cacheable actions
+    [
+        \Subhh\Libconnect\Controller\EzbController::class => 'displayForm'
+    ]
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Libconnect',
+    'EzbSidebar',
+    [
+        \Subhh\Libconnect\Controller\EzbController::class => 'displayMiniForm'
+    ],
+    // non-cacheable actions
+    [
+        \Subhh\Libconnect\Controller\EzbController::class => 'displayMiniForm'
+    ]
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Libconnect',
+    'EzbContact',
+    [
+        \Subhh\Libconnect\Controller\EzbController::class => 'displayContact'
+    ],
+    // non-cacheable actions
+    [
+        \Subhh\Libconnect\Controller\EzbController::class => 'displayContact'
+    ]
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Libconnect',
+    'EzbNew',
+    [
+        \Subhh\Libconnect\Controller\EzbController::class => 'displayNew'
+    ],
+    // non-cacheable actions
+    [
+        \Subhh\Libconnect\Controller\EzbController::class => 'displayNew'
+    ]
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Libconnect',
+    'EzbParticipantsForm',
+    [
+        \Subhh\Libconnect\Controller\EzbController::class => 'displayParticipantsForm'
+    ],
+    // non-cacheable actions
+    [
+        \Subhh\Libconnect\Controller\EzbController::class => 'displayParticipantsForm'
     ]
 );
 
