ae396758f5a3caefa0b60ebf2adb8247a604024b
percolatorWitt <witt.torsten@googlemail.com>
2024-08-27 09:25:28 +0200

    Removed comments.

--- ext_tables.php
+++ ext_tables.php
@@ -2,8 +2,3 @@
 
 defined('TYPO3') or die('Access denied.');
 
-// Add language
-//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_libconnect_domain_model_subject', 'EXT:libconnect/Resources/Private/Language/locallang_csh_tx_libconnect_domain_model_subject.xml');
-
-// Allow to store subject table on standard pages
-//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_libconnect_domain_model_subject');
